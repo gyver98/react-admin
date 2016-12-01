053b40f931f4b930ca549f846c09ad05e87bc007
gyver98 <matthewchoi7@gmail.com>
2016-12-01 13:29:08 +1100

    first react component: homePage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 $ = jQuery = require('jquery');
-var App = console.log("Hello world from Browserify");
+var React = require('react');
+var Home = require('./components/homePage');
 
-module.exports = App;
+
+React.render(<Home />, document.getElementById('app'));
